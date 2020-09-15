441a879e237c7bde6a72fc3009fc45bdb922c532
livelykitten <58162598+livelykitten@users.noreply.github.com>
2020-09-15 15:14:37 +0900

    9/15 lecture note
    https://www.youtube.com/watch?v=uuHHMQgv_9s&feature=youtu.be&ab_channel=KAISTConcurrencyandParallelismLaboratory

--- lock/src/spinlock.rs
+++ lock/src/spinlock.rs
@@ -3,11 +3,18 @@ use core::sync::atomic::{AtomicBool, Ordering};
 use crossbeam_utils::Backoff;
 
 use crate::lock::*;
-
+/*
+    tries to acquire a lock in a spin loop
+    spinlock is a boolean, that can be accessed from multiple threads
+    true - lock, held by someone
+    false - no one is using (no one is locking)
+*/
 pub struct SpinLock {
     inner: AtomicBool,
 }
-
+/*
+    Default - false, since not used
+*/
 impl Default for SpinLock {
     fn default() -> Self {
         Self {
@@ -18,20 +25,35 @@ impl Default for SpinLock {
 
 impl RawLock for SpinLock {
     type Token = ();
-
+/*
+    lock function returns some kind of  proof
+    Token - for other locks have meaningful value
+    Backup sonnze is called, and it waits for the lock to turn off.
+    every consec call to  snooze increases wait time exponentially.
+*/
     fn lock(&self) {
         let backoff = Backoff::new();
-
+/*
+    compare_and_swap: trying to replace Atomicbool 
+    from false to true, and ret true when did it.
+*/
         while self.inner.compare_and_swap(false, true, Ordering::Acquire) {
             backoff.snooze();
         }
     }
-
+/*
+    unlock - stores false, make it able for others to use
+    unsafe - not 100% safe. should'nt unlock lock acquired by others, not me.
+    token acquired by this function needs to be provided to unlock funciton
+*/
     unsafe fn unlock(&self, _token: ()) {
         self.inner.store(false, Ordering::Release);
     }
 }
-
+/*
+    RawTryLock - try to acquire lock
+    discussed later
+*/
 impl RawTryLock for SpinLock {
     fn try_lock(&self) -> Result<(), ()> {
         if !self.inner.compare_and_swap(false, true, Ordering::Acquire) {
