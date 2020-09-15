1ca8bb7de57219e8d4ac0308aa7e4109e1be55a0
livelykitten <58162598+livelykitten@users.noreply.github.com>
2020-09-15 15:52:34 +0900

    9/15 lecture
    https://www.youtube.com/watch?v=uuHHMQgv_9s

--- lock/src/lock.rs
+++ lock/src/lock.rs
@@ -3,6 +3,18 @@ use core::marker::PhantomData;
 use core::mem;
 use core::ops::{Deref, DerefMut};
 
+// high level spinlock no need to care unsafe
+
+/*
+    train RawLock - needs to have Token, lock, unlock
+    (Token from lock and to unlock should match)
+
+    other things user need to guarentee is that the data that is protected
+    should be  related to the lock. 
+
+    those are taken care by high level api
+*/
+
 pub trait RawLock: Default {
     type Token: Clone;
 
@@ -18,28 +30,62 @@ pub trait RawTryLock: RawLock {
     fn try_lock(&self) -> Result<Self::Token, ()>;
 }
 
+/*
+    now lock and data comes in a pair
+    unsafeCell - internal mutability for initial access
+*/
+
 #[repr(C)]
 pub struct Lock<L: RawLock, T> {
     lock: L,
     data: UnsafeCell<T>,
 }
 
+/*
+    lines for making lock type as send or sync
+    T should be sendable since it is accessible by multiple threads
+    no need for T to be Sync. no such case that multiple threads accessing T at the same time
+    (guarenteed by implementation of spinlock)
+    
+    marked unsafe because they are guarenteed by
+    implementation block, not by rust implementation sys
+    manually inspect that locks are send and sync
+*/
+
 unsafe impl<L: RawLock, T: Send> Send for Lock<L, T> {}
 unsafe impl<L: RawLock, T: Send> Sync for Lock<L, T> {}
 
+/*
+    creating Rawlock using Spinlock for type T
+    initial value needs  to  be protected.
+    
+*/
+
 impl<L: RawLock, T> Lock<L, T> {
+    // data: data to protect
     pub fn new(data: T) -> Self {
         Self {
+            // for spinlock, init to false, as seen in rawlock
             lock: L::default(),
             data: UnsafeCell::new(data),
         }
     }
 
+    // destroy lock and get inner data T
+    // when you are given  the total ownership of the lock
     pub fn into_inner(self) -> T {
         self.data.into_inner()
     }
-
+    /*
+        calling  lock acquires inner lock and ret lockguard
+        LockGuard is a proof that you acquired the lock
+        
+    */
     pub fn lock(&self) -> LockGuard<L, T> {
+        /*
+            token partly proves that you acquired the lock,
+            and shouldbe given to the lock function
+        */
         let token = self.lock.lock();
         LockGuard {
             lock: self,
@@ -77,10 +123,23 @@ impl<L: RawLock, T> Lock<L, T> {
         &mut *self.data.get()
     }
 }
-
+/*
+    scope o statically guarenteed to enclose the lifetime of the lock type
+    when the scope is  used in the first  line,
+    Lockguard should be dropped before the lifetime  for s ends.
+    that's the meaning  of 's
+    
+    pub fn lock(&self) -> LockGuard<L, T>
+    scope(lifetime of lock) is given as &self in here
+    so it follows this
+    
+    LockGuard shouldn't outlife the lock - simple!!
+    
+    
+*/
 pub struct LockGuard<'s, L: RawLock, T> {
     lock: &'s Lock<L, T>,
-    token: L::Token,
+    token: L::Token, // token given to the lock function
     _marker: PhantomData<*const ()>, // !Send + !Sync
 }
 
@@ -92,13 +151,21 @@ impl<'s, L: RawLock, T> LockGuard<'s, L, T> {
         self.lock as *const _ as usize
     }
 }
-
+/*
+    when lockguard is dropped(destroyed) underlying lock should be automatically released
+    so this calls unlock function
+*/
 impl<'s, L: RawLock, T> Drop for LockGuard<'s, L, T> {
     fn drop(&mut self) {
+        // unsafe cuz unlock func is unsafe
         unsafe { self.lock.lock.unlock(self.token.clone()) };
     }
+    // internally guarenteed to be safe by programmer, so drop function is safe
 }
 
+/*
+    dereference data
+*/
 impl<'s, L: RawLock, T> Deref for LockGuard<'s, L, T> {
     type Target = T;
 
@@ -106,7 +173,9 @@ impl<'s, L: RawLock, T> Deref for LockGuard<'s, L, T> {
         unsafe { &*self.lock.data.get() }
     }
 }
-
+/*
+    mutably dereference data
+*/
 impl<'s, L: RawLock, T> DerefMut for LockGuard<'s, L, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         unsafe { &mut *self.lock.data.get() }
